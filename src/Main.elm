e88a9dc36b39d154089581d6eed4d0e5179d30de
Emma Cunningham <the.cunning.ham@gmail.com>
2019-01-19 14:37:55 -0800

    Add type for tree & node and create sample tree

--- src/Main.elm
+++ src/Main.elm
@@ -19,6 +19,47 @@ type alias Flags =
     {}
 
 
+type alias RenderedNode =
+    { label : String
+    , details : String
+    }
+
+
+type Tree
+    = Node ( RenderedNode, List Tree )
+
+
+sampleTree : Tree
+sampleTree =
+    let
+        a =
+            RenderedNode "A" "A node"
+
+        b =
+            RenderedNode "B" "B node"
+
+        c =
+            RenderedNode "C" "C node"
+
+        d =
+            RenderedNode "D" "D node"
+
+        e =
+            RenderedNode "E" "E node"
+    in
+    Node
+        ( a
+        , [ Node ( b, [] )
+          , Node
+                ( c
+                , [ Node ( d, [] )
+                  , Node ( e, [] )
+                  ]
+                )
+          ]
+        )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     ( model, Cmd.none )
