fadff06af4cec9f78aad843f087e50689e3df28b
Emma Cunningham <the.cunning.ham@gmail.com>
2019-02-14 20:45:18 -0800

    Add lexical entries dependent on rpresentation

--- src/LexicalEntries.elm
+++ src/LexicalEntries.elm
@@ -7,6 +7,40 @@ type Representation
     | Emoji
 
 
-buffaloMammalGroup : String
-buffaloMammalGroup =
-    "[the group of mammals within the subfamily Bovinae]"
+buffaloMammalGroup : Representation -> String
+buffaloMammalGroup rep =
+    case rep of
+        Verbose ->
+            "[the group of mammals within the subfamily Bovinae]"
+
+        PredicateLogic ->
+            "[Ɩx[bovinae'(x)]]"
+
+        Emoji ->
+            "[the 🐂]"
+
+
+buffaloCityPredicate : Representation -> String
+buffaloCityPredicate rep =
+    case rep of
+        Verbose ->
+            " is from Buffalo"
+
+        PredicateLogic ->
+            "from-buffalo'(x)"
+
+        Emoji ->
+            " is from 🗽 "
+
+
+buffaloVerbPredicate : Representation -> String
+buffaloVerbPredicate rep =
+    case rep of
+        Verbose ->
+            " bullies "
+
+        PredicateLogic ->
+            "bullies'"
+
+        Emoji ->
+            " 👺 "
